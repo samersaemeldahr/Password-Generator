c410f22e70749aab58c6f09412402c5732faba2f
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-02-28 03:56:11 +0300

    Add all files to repo

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+function firstPrompt() {
+  var letters = prompt("hello psychaleppo");
+  alert (letters)
+}
