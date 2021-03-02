517707b811c9e0848b6cf757beb73fc732445b95
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-03-02 06:48:27 +0300

    Add descriptive comments to JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+// Set global variables
 
-
-// Declare global variable
+var charLength;
 var numbers = "0123456789";
 var numbersArray = numbers.split("");
 var lower = "qwertyuiopasdfghjklzxcvbnm";
@@ -9,76 +9,69 @@ var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var upperArray = upper.split("");
 var special = " !#$%&'()*+,-./:;<=>?@[]\"^_`{|}~";
 var specialArray = special.split("");
-var basket = "";
+var basket = [];
 
+// Create a series of actions for password criteria
 
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-var generatePassword = function() {
-  // setting character length
-  var charLength = prompt("How many characters would you like your password to have?  Please choose between '8' and '128' characters.", "8");
-  charLength = parseInt(charLength);
+var passCriteria = function() {
+
+  // Set the password length between 8 and 128 characters
+
+  charLength = prompt("How many characters would you like your password to have?  Please choose between '8' and '128' characters.", "8");
   
   if (isNaN(charLength) || charLength < 8 || charLength > 128) {
     return console.log(alert("The password must be between 8 to 128 characters."));
   }
+
+  // Set a series of confirm boxes to choose a custom criteria
 
   var numbersConfirm = confirm("Would you like to use numbers in your password?");
   var lowerConfirm = confirm("Would you like to use lowercase letters in your password?");
   var upperConfirm = confirm("Would you like to use uppercase letters in your password?");
   var specialConfirm = confirm("Would you like to use special characters in your password?");
 
+  // Add the numbers array to the basket
 
   if (numbersConfirm) {
-  basket = basket.concat(numbers);
+    basket = basket.concat(numbersArray);
   }
+
+  // Add lowercase array to the basket
 
   if (lowerConfirm) {
-    basket = basket.concat(lower);
+    basket = basket.concat(lowerArray);
   }
-    
+  
+  // Add uppercase array to the basket
+
   if (upperConfirm) {
-    basket = basket.concat(upper);
+    basket = basket.concat(upperArray);
   }
 
+  // Add special charectares array to the basket
+
   if (specialConfirm) {
-    basket = basket.concat(special);
+    basket = basket.concat(specialArray);
   } 
   
+  // Input validation to include at least one character type
+
   if (!lowerConfirm && !upperConfirm && !numbersConfirm && !specialConfirm) {
-    return alert("You password must at least have 1 type of characters to generate a password!\nPlease re-generate it.");
+    alert("You password must at least have 1 type of characters to generate a password!\nPlease re-generate it.");
+    return passCriteria();
   }
 }
 
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
-
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-
+var passwordButton = document.querySelector("#password");
 
 // Add event listener to generate button
+
+generateBtn.addEventListener("click", passCriteria);
 generateBtn.addEventListener("click", writePassword);
 
+// Display the password written to the page
 
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
-//
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
